4b53e9da15776878b74505bf745719d454fb18d1
Ethan Wang <renzhong.wang@delphi.com>
2019-08-14 15:09:57 +0800

    更新财务指标下载代码

--- strategy/high_transfer_stock.py
+++ strategy/high_transfer_stock.py
@@ -3,7 +3,7 @@
 # updated : 2019-08-13
 # Author：Ethan Wang
 '''
-选股原理
+高送转策略
 具备高送转预期的个股，具有总市值低、每股公积金高、每股收益大，流通股本少等特点
 股价、每股公积金、每股收益、总股本、流通股本、总市值
 将公积金大于等于5元，每股收益大于等于5毛，流通股本在3亿以下，总市值在100亿以内作为高送转预期目标
@@ -15,27 +15,38 @@ pd.set_option( 'display.max_columns', None )  # 显示所有列
 pd.set_option( 'display.max_rows', None )  # 显示所有行
 pd.set_option( 'max_colwidth', 200 )  # 设置value的显示长度为100，默认为50
 
-ts.set_token( '35b181fd609f03009de99d5cb8ba6ac391ce96e2dc8b31c506c33ddb' )
+ts.set_token( '33c9dc31a0d5e549125e0322e6142137e2687212b171f8dde4f21668' )
 pro = ts.pro_api()
 
 # 设置今天日期
 # today = date.today().strftime( "%Y%m%d" )
 
-today = '20190812'
+today = '20190813'
 
 #设置参数边界
 ht_bound = {'股价(元)': 25,
             '每股资本公积(元)': 2,
-            '每股收益(元)': 0.2,
+            '每股收益(元)': 0.5,
             '总股本(亿)': 3,
             '流通股本(亿)': 1,
             '总市值(亿)': 100}
+
+# 股票名称列表
+# 接口：stock_basic
+share_basic = pro.stock_basic(exchange='', list_status='L', fields='ts_code,symbol,name,industry,list_date')
+share_basic.to_excel( "const_share_basic.xlsx" )
 
 # 每日指标
 # 接口：daily_basic
 basic = pro.daily_basic( ts_code='', trade_date=today,
                          fields='ts_code, close, pe, pe_ttm, pb, total_share, float_share, total_mv, circ_mv, turnover_rate_f' )
 basic.to_excel( "basic.xlsx" )
+
+
+merge_basic =  pd.merge( share_basic, basic, on='ts_code', sort=False,
+                              left_index=False, right_index=False, how='left' )
+merge_basic.to_excel( "merge_basic.xlsx" )
+
 
 # 日线行情
 # 接口：daily
@@ -44,8 +55,8 @@ daily = pro.daily( ts_code='', trade_date=today,
 daily.to_excel( "daily.xlsx" )
 
 # 合并每日基本信息
-merge_basic_daily = pd.merge( basic, daily, on='ts_code', sort=False,
-                              left_index=False, right_index=False, how='inner' )
+merge_basic_daily = pd.merge( merge_basic, daily, on='ts_code', sort=False,
+                              left_index=False, right_index=False, how='left' )
 merge_basic_daily.to_excel( 'merge_basic_daily.xlsx' )
 
 # 财务指标数据fina_indicator
@@ -60,7 +71,7 @@ indicators = pd.read_excel( "const_indicator.xlsx" )
 #merge_basic_daily['close'] = merge_basic_daily.apply( lambda x: x.pre_close if x.close == 0 else x.close, axis=1 )
 
 # 选取股票代码,当前价格,流通股本,总股本,流通市值,总市值
-ht_merge_basic_daily = merge_basic_daily[['ts_code', 'close_x', 'float_share', 'total_share', 'circ_mv', 'total_mv']]
+ht_merge_basic_daily = merge_basic_daily[['ts_code','name', 'close_x', 'float_share', 'total_share', 'circ_mv', 'total_mv']]
 
 # 选取股票代码,每股资本公积,每股收益
 ht_indicators = indicators[['ts_code', 'capital_rese_ps', 'eps']]
--- strategy/indicator.py
+++ strategy/indicator.py
@@ -7,19 +7,14 @@
 若已经运行并生成indicator.xlsx，则不需要再次运行
 此文件仅在上市公司更新财报时才需要运行，更新indicator.xlsx
 parameter：START_DATA, END_DATA,
-today设置为前一天
 '''
 import time
 
 import pandas as pd
 import tushare as ts
 
-ts.set_token( '35b181fd609f03009de99d5cb8ba6ac391ce96e2dc8b31c506c33ddb' )
+ts.set_token( '33c9dc31a0d5e549125e0322e6142137e2687212b171f8dde4f21668' )
 pro = ts.pro_api()
-
-# 设置今天日期
-# today = date.today().strftime( "%Y%m%d" )
-today = '20190812'
 
 # 选取财报时间段
 START_DATA = '20190301'
@@ -29,16 +24,25 @@ indicator = pd.DataFrame()
 index = int()
 
 # 获取股票代码
-basic = pro.daily_basic( ts_code='', trade_date=today, fields='ts_code' )
+share_basic = pro.stock_basic( exchange='', list_status='L', fields='ts_code,symbol,name,industry,list_date' )
 
-for str in list( basic['ts_code'] ):
-    test = pro.fina_indicator( ts_code=str, start_date=START_DATA, end_date=END_DATA,
-                               fields='ts_code, eps, dt_eps, bps, roe, roe_waa, roe_dt, capital_rese_ps,'
-                                      'undist_profit_ps, ann_date, end_date' )
-    indicator = indicator.append( test )
-    index = index + 1
-    print( index )
-    if (index % 80 == 0):
-        time.sleep( 30 )
+num = len( share_basic )
+print( "上市公司总数：%d" % num )
+# 获取上市公司财务指标数据
+for _ in range( 3 ):
+    try:
+        for str in list( share_basic['ts_code'] ):
+            test = pro.fina_indicator_vip( ts_code=str, start_date=START_DATA, end_date=END_DATA,
+                                           fields='ts_code, eps, dt_eps, bps, roe, roe_waa, roe_dt, capital_rese_ps,'
+                                                  'undist_profit_ps, ann_date, end_date' )
+            indicator = indicator.append( test )
+            index = index + 1
+            print( '正在下载第(%d)条,%s, %s' % (index, share_basic.iloc[index - 1, 0], share_basic.iloc[index - 1, 2]) )
+            if (index == num):
+                print( "下载完毕！！！" )
+                break
+    except:
+        time.sleep( 2 )
+        print( "异常了！！！！" )
 
 indicator.to_excel( "const_indicator.xlsx" )
